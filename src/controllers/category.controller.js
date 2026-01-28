1c29817f5bae0b3807d82a981bf1f5d91391fcc2
kadalideepak <kadali.deepak.3@gmail.com>
2026-01-28 20:15:31 +0530

    catogiry

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -17,7 +17,7 @@ exports.createCategory = async (req, res) => {
   }
 };
 
-// GET ALL
+// GET ALLL
 exports.getAllCategories = async (req, res) => {
   const categories = await Category.findAll();
   res.json(categories);
